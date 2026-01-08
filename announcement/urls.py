1eb78b49306bda3945dc05d49bebbae391a42c99
denys09310 <denys09310@gmail.com>
2026-01-08 18:23:15 +0000

    .

---
+++ announcement/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Визнач тут свої URL-шляхи
+    
+]
--- logika_portal/urls.py
+++ logika_portal/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('accounts.urls')),
+    path('announcement/', include('announcement.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
