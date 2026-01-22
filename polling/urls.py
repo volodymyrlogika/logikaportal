e05188e4fdf332afdf34e9875339b48af20e1bae
maksmaks21 <maksroks777@gmai.com>
2026-01-22 21:02:52 +0200

    appdated polling

--- polling/urls.py
+++ polling/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.PollingListView.as_view(), name="polling_list"),
+    path("<int:pk>/", views.PollingDetailView.as_view(), name="polling_detail"),
 ]
