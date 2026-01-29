e9ca6366ddd5cef7b6dd114b185d2d2e74d377e4
denys09310 <denys09310@gmail.com>
2026-01-29 18:13:37 +0000

    final

--- logika_portal/settings.py
+++ logika_portal/settings.py
@@ -129,11 +129,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Login configuration
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-<<<<<<< HEAD
-LOGOUT_REDIRECT_URL = '/'
-=======
 LOGOUT_REDIRECT_URL = '/login/'
->>>>>>> ann2
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
