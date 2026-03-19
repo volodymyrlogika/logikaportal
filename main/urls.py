d82808d306044e6d079a4994512eb733cffd55f9
volodymyrlogika <volodymyrlogika@gmail.com>
2026-03-19 19:56:59 +0200

    fix login

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('', TopicListView.as_view(), name='topic_list'),
     path('topic/<int:topic_id>/', topic_detail, name='topic_detail'),
     path('comment/delete/<int:comment_id>/', delete_comment, name='delete_comment'),
-
-    # Login/Logout
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
