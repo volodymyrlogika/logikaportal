9715857d869f64cc0c3667082af9a3296495dc56
volodymyrlogika <volodymyrlogika@gmail.com>
2026-03-19 20:28:33 +0200

    upd settings to production

--- logika_portal/urls.py
+++ logika_portal/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include('accounts.urls')),
     path('polling/', include('polling.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
