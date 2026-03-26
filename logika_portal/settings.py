935182fdf70e6277a01f1348c9352cf425e65389
volodymyrlogika <volodymyrlogika@gmail.com>
2026-03-26 19:41:29 +0200

    prepare to deployment

--- logika_portal/settings.py
+++ logika_portal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5rh=#tul4uustjy#bekm_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]   # Дозволити всі хости 
 
 
 # Application definition
